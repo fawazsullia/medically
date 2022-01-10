eb3d364098904dd6b4aa00b1cdf08ef7df28efa6
Fawaz Sullia <fawazsullia@gmail.com>
2022-01-10 11:38:25 +0400

    feature : make it possible to download the diles

--- client/src/components/Uploads.js
+++ client/src/components/Uploads.js
@@ -10,7 +10,7 @@ const [uploadDetails, setuploadDetails] = useState([])
 useEffect(() => {
   fetch(`${appConfig.baseUrl}/file/get-uploads/${patientId}`)
   .then((res)=> res.json())
-  .then((response)=> { console.log(response.header)   })
+  .then((response)=> { setuploadDetails(response.uploads) })
   return () => {
     setuploadDetails([])
   }
@@ -22,7 +22,25 @@ useEffect(() => {
       handleCloseUploads()
   }
 
+//download the file when clicked on placeholder
+const handleDownloads = (e)=> {
+  let fileName = e.target.nodeName === "DIV" ? e.target.id : e.target.parentNode.id;
+  fetch(`http://localhost:5000/file/get-file/${fileName}`)
+  .then((res)=> res.blob())
+  .then((response)=> {
+    console.log(response)
+    let url = window.URL.createObjectURL(response)
+   let anchor = document.createElement("a");
+   anchor.href = url;
+   //figure out a way to make the download file name the title of the file
+   anchor.download = `${fileName}`;
+   anchor.click();
+  })
+  .catch((e)=> console.log(e))
+ 
+  
 
+}
 
 
 
@@ -37,7 +55,7 @@ useEffect(() => {
 { !uploadDetails.length ? <div><p>No uploads yet...</p></div> :
   
   <div className={uploadStyle.uploadDiv}>
-    {uploadDetails.map((data) => {return <div className={uploadStyle.card}>
+    {uploadDetails.map((data) => {return <div onClick={handleDownloads} className={uploadStyle.card} id={data.uploadFileName} key={data.uploadFileName}>
         <h5>{data.uploadTitle}</h5>
         <p>{data.uploadDate}</p>
         <h4 className={uploadStyle.download}>download</h4>
